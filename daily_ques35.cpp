60681266a129b8baafc3640dfb1ae4ba60191d3e
Vinay Kumar <vinaydayma38@gmail.com>
2026-06-08 10:56:29 +0530

    Question 2161: 06/08/2026

---
+++ daily_ques35.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> pivotArray(vector<int>& nums, int pivot) {
+        vector<int> res;
+        res.reserve(nums.size());
+        
+        for (int x : nums) {
+            if (x < pivot) {
+                res.push_back(x);
+            }
+        }
+        
+        for (int x : nums) {
+            if (x == pivot) {
+                res.push_back(x);
+            }
+        }
+        
+        for (int x : nums) {
+            if (x > pivot) {
+                res.push_back(x);
+            }
+        }
+        
+        return res;
+    }
+};
